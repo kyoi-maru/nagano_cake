8c558c6b4811e745ae2668882b153073fd6b4466
kyoi-maru <99070966+kyoi-maru@users.noreply.github.com>
2022-03-22 07:52:06 +0000

    Add モデル

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "70c16685503b4739a0fbd43e68123a51.vfs.cloud9.us-east-1.amazonaws.com"
 end
